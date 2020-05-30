9fcbe923cf46479d9db0ed788eaea1b650ae4e31
sergioviniciuss <sergioviniciuss@gmail.com>
2020-05-30 22:59:06 +0200

    Feat(Button): Creates styled component for button

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const ExampleButton = styled.button`
+  background: orange;
+  color: white;
+  font-size: 2rem;
+  border-radius: 5px;
+  border-color: transparent;
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { ExampleButton } from "../components/Button"
 
 export default function Home() {
   return (
     <Layout>
       <h1>Hello from gatsby</h1>
+      <ExampleButton>Click me</ExampleButton>
     </Layout>
   )
 }
