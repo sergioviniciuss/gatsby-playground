a8dceb07c763014d6bc2888ab40c2fa4174c995d
sergioviniciuss <sergioviniciuss@gmail.com>
2020-05-30 21:41:06 +0200

    WIP(navigation): Testing link with reloading

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,6 +4,7 @@ const Blog = () => {
   return (
     <div>
       <h1>this is our blog page</h1>
+      <a href="/">Home page</a>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      Hello world!
+      <div>
+        <a href="/blog/">blog page</a>
+      </div>
+      <a href="https://www.gatsbyjs.org/">Gatsby docs</a>
+    </div>
+  )
 }
